f627581d8d8d234dc9796de8cc7e1c9b9ea2bcd5
Tyler Bakker <tylerbakker483@gmail.com>
2023-01-15 15:47:55 -0500

    changes made to the JS code

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -18,8 +18,12 @@ function todoLive(){
                 window.open('https://github.com/Tyler2125/Todolist')
                 }
 
-
-
+function pixelCode(){
+        window.open("https://github.com/Tyler2125/PixelMaker")
+}
+function pixelLive(){
+        window.open("https://tyler2125.github.io/PixelMaker/")
+}
   return (
         // Id is used for navigation
     <div id='Projects' className=' each-section'><h1>Projects</h1>
@@ -44,8 +48,8 @@ function todoLive(){
         <h3>Pixel Art Maker</h3>
         <img className='project-img' src={PixelArt} alt='Pixel Art'/>
         <p>A Dynamic Pixel Art maker that allows you to set the canvas size and choose the colors you would like to draw with.This project was created with HTML,CSS and Vanilla JS. This project also does not save the design when refreshed.</p>
-        <button className='project-button'>View Code</button>
-<button className='project-button'>Live Demo</button>
+        <button className='project-button' onClick={pixelCode}>View Code</button>
+<button className='project-button' onClick={pixelLive}>Live Demo</button>
         </div>
 </div>
 
